246b74244c184fbf0118e302bad418f8f0145d3a
TessLMAO <110305665+TessLMAO@users.noreply.github.com>
2022-12-09 21:42:10 -0300

    Add files via upload

---
+++ assets/preload/scripts/script.lua
@@ -0,0 +1,6 @@
+function onCreate()
+    setPropertyFromClass('GameOverSubstate', 'riverflustered'); --Character json file for the death animation
+    setPropertyFromClass('GameOverSubstate', 'fnf_loss_sfx'); --put in mods/sounds/
+    setPropertyFromClass('GameOverSubstate', 'gameOver'); --put in mods/music/
+    setPropertyFromClass('GameOverSubstate', 'gameOverEnd'); --put in mods/music/
+end
